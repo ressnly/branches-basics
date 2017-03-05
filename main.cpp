6d1bf482d583c0ad80a705f0c665a2c3d9ecf673
Богдан <ress@ress.home>
2017-03-05 18:38:25 +0700

    Feature #1: init massive

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
+#define N 10
+
+void initMassive(int a[], int n);
 
 int main()
 {
+	int a[N];
+	initMassive(a,N);
 	std::cout << "All right, man" << std::endl;
 	return 0;
+}
+
+void initMassive(int a[], int n)
+{
+	for(int i = 0; i < n; ++i) *(a+i) = i;
 }
