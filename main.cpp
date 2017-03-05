4d0cdf42801958ee1269047cdd3711d2604ab845
Богдан <ress@ress.home>
2017-03-05 18:39:43 +0700

    Feature #2: Sorting massive

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #define N 10
 
 void initMassive(int a[], int n);
+void shackersort(int a[], int n)
 
 int main()
 {
@@ -14,4 +15,35 @@ int main()
 void initMassive(int a[], int n)
 {
 	for(int i = 0; i < n; ++i) *(a+i) = i;
+}
+
+void shackersort(int a[], int n)
+{
+    int k = n - 1,
+        r = n - 1,
+        l = 0;
+
+    do {
+        for(int i = r; i > l; i--)
+        {
+            if(a[i] < a[i-1])
+            {
+                swap(a[i],a[i-1]);
+                k = i;
+            }
+        }
+        l = k;
+
+        for(int i = l; i < r; i++)
+        {
+            if(a[i] > a[i+1])
+            {
+                swap(a[i],a[i+1]);
+                k = i;
+            }
+        }
+        r = k;
+    } while(l < r);
+
+    return;
 }
